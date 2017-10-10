ad60e1f3155773a998c534e6856a2f758f618e9e
willstearn <willstearn>
2017-10-10 14:14:49 +0100

    css

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// console.log('loaded');
+
+// pick random box
+// add class to box
+// add click event to box
+// after time, remake class and click off box
+
+// >> Loop >>
+// After first iteration, inside loop add:
+// - Interval
+// - Timer
